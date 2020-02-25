e222f8670f0f5a298a4488f68775070a81766307
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-25 12:28:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/")) #takes string of HTML returned by open-uri's open
+#method and converts into a NodeSet (or nested nodes: parent and child elements ie.hashes)
+#this will puts out our html into a doc
+#Nokogiri allows us to iterate over our code and use brackets and
+#dot notation to access our code
+doc.css(".headline-26OIBN").text #here we are using our css selector to grab the span of the title 

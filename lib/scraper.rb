996f49b77b101e39e332984dab3f1c7cca519d58
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-25 12:30:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,5 +14,5 @@ puts doc.css(".headline-26OIBN") #prints HTML
 
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
- 
+
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")

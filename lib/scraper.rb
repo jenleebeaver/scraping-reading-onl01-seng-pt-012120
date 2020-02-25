fee80756f2d2a14231c79e8eb4d2a143abd2b02d
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-25 12:34:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,3 +18,10 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+#here we iterate over the variable we are looking for 
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+courses.each do |course|
+  puts course.text.strip
+end

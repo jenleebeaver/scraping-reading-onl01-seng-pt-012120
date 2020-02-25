673bf9a71d8e6eca509bedba9d6c253088415665
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-25 12:30:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,3 +11,8 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/")) #takes string of HTML 
 doc.css(".headline-26OIBN").text #here we are using our css selector to grab the span of the title
 
 puts doc.css(".headline-26OIBN") #prints HTML
+
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
